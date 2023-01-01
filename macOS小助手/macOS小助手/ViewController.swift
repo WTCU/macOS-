8c353f5f1669fcc6117745f8fbf3259600aa9f0e
庞玺桐 <pangxitong@icloud.com>
2023-01-01 08:45:42 +0800

    创建Xcode项目

---
+++ macOS小助手/macOS小助手/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOS小助手
+//
+//  Created by mac on 2023/1/1.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
